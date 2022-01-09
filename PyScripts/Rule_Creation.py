f92291e90d2003587655f4527f8050a9d849d129
zeldabedabomb <88691158+zeldabedabomb@users.noreply.github.com>
2022-01-08 20:26:55 -0500

    Finished pie graph.
    work on setting up rules column data correctly.
    
    add dynamic graph to show the probability of choices.

--- PyScripts/Rule_Creation.py
+++ PyScripts/Rule_Creation.py
@@ -2,7 +2,10 @@ import pandas as pd
 import csv
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
-
+'''
+author: James Mills date: 01/2022
+This script was ran to complete the rules association creation for the item set.
+'''
 with open('./Data/transaction_data.csv', 'r', newline='') as f:
     reader = csv.reader(f, delimiter=',')
     next(reader)
@@ -12,7 +15,7 @@ te_ary = te.fit(sample_data).transform(sample_data)  # take transaction data and
 df = pd.DataFrame(te_ary, columns=te.columns_)
 print(len(df))  # length of initial hot-encoded pandas dataframe
 frequent_1 = apriori(df, min_support=0.0025654, use_colnames=True)  # support for at least 10 of the 3898 transactions
-print(len(frequent_1))  # length of initial rules
+print(len(frequent_1))  # length of initial rules with 167 total unique items
 frequent_1.head()
 rules_1 = association_rules(frequent_1, metric="confidence", min_threshold=0.5)
 # adds a cutoff of at least 50% confidence
