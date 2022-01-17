d2b9f859a81ca013263573865d6568ab27c9a2b5
zeldabedabomb <88691158+zeldabedabomb@users.noreply.github.com>
2022-01-16 19:12:41 -0500

    Fixed last graph. Fixed nbextensions. TODO: remove total bar on the first graph.

--- PyScripts/RuleCreation.py
+++ PyScripts/RuleCreation.py
@@ -15,8 +15,8 @@ with open('../Data/transaction_data.csv', 'r', newline='') as f:
 te = TransactionEncoder()
 te_ary = te.fit(sample_data).transform(sample_data)  # take transaction data and transform into df
 df = pd.DataFrame(te_ary, columns=te.columns_)
-print(len(df))  # length of initial hot-encoded pandas dataframe 3897
-frequent_1 = apriori(df, min_support=0.0025654, use_colnames=True)  # support for at least 10 of the 3897 transactions
+print(len(df))  # length of initial hot-encoded pandas dataframe 3898
+frequent_1 = apriori(df, min_support=0.0025654, use_colnames=True)  # support for at least 10 of the 3898 transactions
 print(len(frequent_1))  # length of initial rules with 167 total unique items 34612
 frequent_1.head()
 rules_1 = association_rules(frequent_1, metric="confidence", min_threshold=0.5)
