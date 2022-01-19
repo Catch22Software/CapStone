f4f20b23190abc447e75997dd9d414041dfd52a4
zeldabedabomb <88691158+zeldabedabomb@users.noreply.github.com>
2022-01-18 22:47:39 -0500

    Final Commit Before Submission

--- PyScripts/DataCleaning.py
+++ PyScripts/DataCleaning.py
@@ -10,7 +10,9 @@ df = pd.read_csv('../Data/Groceries_dataset.csv')  # reads in initial dataset
 df1 = df.drop(columns=["Date"])  # drops unneeded Date column
 df1.sort_values(by=['Member_number'], inplace=True)  # sorts by member number
 df1.reset_index(inplace=True)
-df1.drop(columns=['index'],inplace=True)  # resets the index to map lowest to highest member numbers
+df1.drop(columns=['index'], inplace=True)  # resets the index to map lowest to highest member numbers
+df1['itemDescription'].isnull().values.any()  # checks for any null values
+df1['Member_number'].isnull().values.any()
 grouped = df1.groupby('Member_number')['itemDescription'].apply(list)  # creates list of items purchased by member
 transactions = grouped.to_list()
 file = open('../Data/transaction_data.csv', 'w', newline='')  # saves resulting list to csv file for rule creation
